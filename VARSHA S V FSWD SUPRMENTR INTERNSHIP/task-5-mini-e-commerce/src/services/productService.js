9f62da7f2249bcd2527fb1cf5d7f73a0ec29ca55
Varsha <varsha@example.com>
2026-03-27 16:41:43 +0530

    Remove VARSHA S V FSWD SUPRMENTR INTERNSHIP folder from tracking

--- VARSHA S V FSWD SUPRMENTR INTERNSHIP/task-5-mini-e-commerce/src/services/productService.js
+++
@@ -1,8 +0,0 @@
-const products = [
-  { id: 1, name: 'Smartphone', price: 24999 },
-  { id: 2, name: 'Backpack', price: 1999 },
-  { id: 3, name: 'Sneakers', price: 3299 },
-  { id: 4, name: 'Watch', price: 5499 }
-]
-
-export const getProducts = () => products
